e43fbc264b1ae2eb645e93b24499aba1c4bde3d3
ENIGMA <i128saagaye.v@inbox.ru>
2025-05-16 00:37:40 +0400

    fix: Update homepage and base path for deployment in package.json and vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/rasha-coffeeshop-template/',
+  base: '/rasha-coffeeshop/',
   plugins: [react()],
   resolve: {
     alias: {
